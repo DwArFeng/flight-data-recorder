fd649d02ad154caad1e2ed10cd19494383ba6aa5
DwArFeng <915724865@qq.com>
2020-11-26 15:06:41 +0800

    为 PointNotExistsException 添加文档注释
    Signed-off-by: DwArFeng <915724865@qq.com>

--- fdr-stack/src/main/java/com/dwarfeng/fdr/stack/exception/PointNotExistsException.java
+++ fdr-stack/src/main/java/com/dwarfeng/fdr/stack/exception/PointNotExistsException.java
@@ -2,6 +2,12 @@ package com.dwarfeng.fdr.stack.exception;
 
 import com.dwarfeng.subgrade.stack.bean.key.LongIdKey;
 
+/**
+ * 数据点不存在异常。
+ *
+ * @author DwArFeng
+ * @since 1.1.0
+ */
 public class PointNotExistsException extends Exception {
 
     private static final long serialVersionUID = 5584249920639455084L;
